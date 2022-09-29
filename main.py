c49727ab730eb2aff257d0ca2d3617e53aef7a81
LUCAS VIEIRA DA SILVA <lucas.vsilva59@senacsp.edu.br>
2022-09-28 21:55:26 -0300

    up2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, render_template, request
 from model import model
 import this
@@ -7,25 +8,27 @@ this.modelo = model()
 this.msg = ""
 this.campo = ""
 this.dado = ""
-#espaço cliente
+#espaço cuidador
 this.cod = 0
+this.nome = ""
 this.email = ""
 this.telefone = ""
 this.cpf = ""
 this.sexo = ""
 this.rg = ""
-this.user = ""
+this.usu = ""
 this.pswd = ""
-#espaço cuidador
+this.endereco = ""
+#espaço cliente
 this.cod1 = 0
-this.nome = ""
+this.nome1 = ""
 this.apelido = ""
 this.sus = ""
 this.rg1 = ""
 this.cpf1 = ""
 this.data = ""
 this.sexo1 = ""
-this.endereco = ""
+this.endereco1 = ""
 this.data = ""
 this.uf = ""
 this.nacionalidade = ""
@@ -35,7 +38,7 @@ this.escolaridade = ""
 this.etnia = ""
 this.religiao = ""
 this.email1 = ""
-this.user1 = ""
+this.usu1 = ""
 this.pswd1 = ""
 #espaço monitoramento
 this.pressao = ""
@@ -43,46 +46,99 @@ this.diabete = 0
 this.dieta = ""
 this.kilo = 0
 this.altura = 0
-this.cod1 = 0
-#espaço agenda
 this.cod2 = 0
+#espaço agenda
+this.cod3 = 0
 this.exame = ""
 this.consulta = ""
 pessoa = Flask(__name__)
 
+#Cliente abaixo
+
 @pessoa.route('/', methods=['GET','POST'])
-def cadastrar():
+def cadastrarCliente():
     if request.method == 'POST':
-        this.nome     = request.form['tNovoNome']
-        this.telefone = request.form['tNovoTelefone']
-        this.endereco = request.form['tNovoEndereco']
-        this.data     = request.form['tNovaData']
-        this.dados    = this.modelo.inserir(this.nome, this.telefone, this.endereco, this.data)
+        this.nome1     =      request.form['tNewNome']
+        this.apelido  =   request.form['tNewApelido'] 
+        this.sus      =       request.form['tNewSus']
+        this.rg1       =        request.form['tNewRg']
+        this.cpf1      =       request.form['tNewCpf']
+        this.data     =      request.form['tNewData'] 
+        this.sexo1    =      request.form['tNewSexo']
+        this.uf       =        request.form['tNewUf']
+        this.nacionalidade = request.form['tNewNacionalidade']
+        this.alfabetizado  = request.form['tNewAlfabetizado'] 
+        this.escolaridade  = request.form['tNewEscolaridade']
+        this.etnia     =         request.form['tNewEtnia']
+        this.religiao  =      request.form['tNewReligiao']
+        this.email1     =     request.form['tNewEmail']
+        this.usu1      =      request.form['tNewUsu']
+        this.pswd1     =      request.form['tNewPswd']
+        this.endereco1  =      request.form['tNewEndereco1']
+        this.dados     = this.modelo.inserir(this.nome1, this.apelido, this.sus, this.rg, this.cpf1, this.data, this.sexo1, this.uf, this.nacionalidade, this.alfabetizado, this.escolaridade, this.etnia, this.religiao, this.email1, this.usu1, this.pswd1, this.endereco1)
     return render_template('index.html', titulo="Página Principal", resultado=this.dados)
 
-@pessoa.route('/consultar.html', methods=['GET','POST'])
-def consultarTudo():
+@pessoa.route('/', methods=['GET','POST'])
+def consultarCliente():
     if request.method == 'POST':
-        this.codigo   = request.form['tNovoCodigo']
-        this.msg = this.modelo.consultar(this.codigo)
+        this.cod1   = request.form['tNewCod1']
+        this.msg = this.modelo.consultar(this.cod1)
     return render_template('consultar.html', titulo="Consultar", dados=this.msg)
 
-@pessoa.route('/atualizar.html', methods=['GET','POST'])
-def atualizarDado():
+@pessoa.route('/', methods=['GET','POST'])
+def atualizarCliente():
     if request.method == 'POST':
-        this.codigo = request.form['tCodigo']
+        this.cod1 = request.form['tCod1']
         this.campo = request.form['tCampo']
         this.dado = request.form['tDado']
-        this.msg = this.modelo.atualizar(this.codigo, this.campo, this.dado)
+        this.msg = this.modelo.atualizar(this.cod1, this.campo, this.dado)
     return render_template('atualizar.html', titulo="Atualizar", dados=this.msg)
 
-@pessoa.route('/excluir.html', methods=['GET','POST'])
-def excluirDado():
+@pessoa.route('/', methods=['GET','POST'])
+def excluirCliente():
     if request.method == 'POST':
-        this.codigo = request.form['tCodigo']
-        this.msg = this.modelo.excluir(this.codigo)
+        this.cod1 = request.form['tCod1']
+        this.msg = this.modelo.excluir(this.cod1)
     return render_template('excluir.html', titulo="Excluir", dados=this.msg)
 
+#Cuidador  abaixo
+
+@pessoa.route('/', methods=['GET', 'POST'])
+def cadastrarCuidador():
+    if request.method == 'POST':
+        this.email = request.form['tNewEmail']
+        this.telefone = request.form['tNewTelefone']
+        this.cpf = request.form['tNewCpf']
+        this.sexo = request.form['tNewSexo']
+        this.rg = request.form['tNewRg']
+        this.usu = request.form['tNewUsu']
+        this.pswd = request.form['tNewPswd']
+        this.endereco  =      request.form['tNewEndereco']
+        this.dados     = this.modelo.inserir(this.email, this.telefone,this.cpf,this.sexo,this.rg,this.usu,this.pswd)
+    return render_template('index.html', titulo="Página Principal", resultado=this.dados)
+
+@pessoa.route('/', methods=['GET','POST'])
+def consultarCuidador():
+    if request.method == 'POST':
+        this.cod   = request.form['tNovoCod']
+        this.msg = this.modelo.consultar(this.cod)
+    return render_template('consultar.html', titulo="Consultar", dados=this.msg)
+
+@pessoa.route('/', methods=['GET','POST'])
+def atualizarCuidador():
+    if request.method == 'POST':
+        this.cod = request.form['tCod']
+        this.campo = request.form['tCampo']
+        this.dado = request.form['tDado']
+        this.msg = this.modelo.atualizar(this.cod, this.campo, this.dado)
+    return render_template('atualizar.html', titulo="Atualizar", dados=this.msg)
+
+@pessoa.route('/', methods=['GET','POST'])
+def excluirCuidador():
+    if request.method == 'POST':
+        this.cod = request.form['tCod']
+        this.msg = this.modelo.excluir(this.cod)
+    return render_template('excluir.html', titulo="Excluir", dados=this.msg)
 
 if __name__ == '__main__':
     pessoa.run(debug=True, port=5000)
--- model.py
+++ model.py
@@ -7,78 +7,118 @@ class model:
         self.db_connection = self.db_connection.conectar()#Conecto ao banco de dados
         self.con = self.db_connection.cursor()#Navega no meu banco
         
-    def inserir(self, nome, apelido, telefone, endereco, dataDeNascimento):
+#Cliente       
+
+    def inserirFormulario(self, nome, apelido, sus, rg, cpf, dataDeNascimento, sexo, uf, nacionalidade, alfabetizado, escolaridade, etnia, religiao, endereco):
         try:
-            sql = "Insert into person(codigo, nome, apelido, telefone, endereco, dataDeNascimento) values('','{}','{}','{}','{}')".format(nome, telefone, endereco, dataDeNascimento)
+            sql = "Insert into cliente(cod, nome, apelido, sus, rg, cpf, dataDeNascimento, sexo, uf, nacionalidade, alfabetizado, escolaridade, etnia, religiao, endereco) values('','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(nome, apelido, sus, rg, cpf, dataDeNascimento, sexo, uf, nacionalidade, alfabetizado, escolaridade, etnia, religiao, endereco)
             self.con.execute(sql)
             self.db_connection.commit()
             return "{} Inserido!".format(self.con.rowcount)
         except Exception as erro:
             return erro
         
-    def consultar(self, cod):
+    def consultarFormulario(self, cod):
         try:
-            sql = "select * from person where codigo ='{}'".format(codigo)
+            sql = "select * from cliente where cod ='{}'".format(cod)
             self.con.execute(sql)
             msg = ""
             
-            for(cod, nome, telefone, endereco, dataDeNascimento) in self.con:
-                msg = msg + "\nCódigo: {}, Nome: {}, Telefone: {}, Endereço: {}, Nascimento: {}".format(codigo, nome, telefone, endereco, dataDeNascimento)
+            for(nome, apelido, sus, rg, cpf, dataDeNascimento, sexo, uf, nacionalidade, alfabetizado, escolaridade, etnia, religiao, endereco) in self.con:
+                msg = msg + "\nCódigo: {}, Nome: {}, Apelido: {}, NºSUS: {}, RG: {}, CPF: {}, Nascimento: {}, Sexo: {}, UF: {}, Nacionalidade: {}, Alfabetizado: {}, Escolaridade: {}, Etnia: {}, Religião: {}, Endereço: {}".format(nome, apelido, sus, rg, cpf, dataDeNascimento, sexo, uf, nacionalidade, alfabetizado, escolaridade, etnia, religiao, endereco)
             return msg
         except Exception as erro:
             return erro
         
-    def atualizar(self, cod, campo, novoDado):
+    def atualizarFormulario(self, cod, campo, novoDado):
         try:
-            sql = "update person set {} = '{}' where codigo = '{}'".format(campo, novoDado, cod)
+            sql = "update cliente set {} = '{}' where cod = '{}'".format(campo, novoDado, cod)
             self.con.execute(sql)
             self.db_connection.commit()
             return "{} linha atualizada!".format(self.con.rowcount)
         except Exception as erro:
             return erro
         
-    def excluir(self, cod):
+    def excluirFormulario(self, cod):
         try:
-            sql = "delete from person where codigo = '{}'".format(cod)
+            sql = "delete from cliente where cod = '{}'".format(cod)
             self.con.execute(sql)
             self.db_connection.commit()
             return "{} linha excluida!".format(cod) 
         except Exception as erro:
             return erro
+        
+#Metodo Login Cliente
 
-    def cad(self, user, pswd, email, cpf):
+    def cad1(self, usu1, pswd1, email, cpf):
         try:
-            sql = "Insert into cuidador(codigo, user, pswd, email, cpf) values('','{}','{}','{}','{}')".format(user, pswd, email, cpf)
+            sql = "Insert into cliente(cod, usu, pswd, email, cpf) values('','{}','{}','{}','{}')".format(usu1, pswd1, email, cpf)
             self.con.execute(sql)
             self.db_connection.commit()
             return "{} Inserido!".format(self.con.rowcount)
         except Exception as erro:
             return erro
         
-    def loginInsert(self,user, pswd, userInput, pswdInput):
+    def loginInsert1(self, usu1, pswd1, usu1Input, pswd1Input):
         try:
-            sql = 'select * from login'
+            sql = 'select * from cliente'
             self.con.execute(sql)
-            for (codigo, user, pswd) in con:
-                if user and pswd == userInput and pswdInput:
+            for (cod, usu1, pswd1) in self.con:
+                if usu1 and pswd1 == usu1Input and pswd1Input:
                     return True
             else:
                 print('\nUsuário ou senha incorretos!')
             return False
         except Exception as erro:
             return erro
-    def loginValid(user, pswd, userInput, pswdInput):
+
+    def loginValid1(self, usu1, pswd1, usu1Input, pswd1Input):
         try:
-            if loginInsert(user, pswd, userInput, pswdInput) == True:
+            if self.loginInsert1(usu1, pswd1, usu1Input, pswd1Input) == True:
                 print('Logado com Sucesso !')
                 print('\n')
-                menu.operacao()
             else:
                 print('Login e Senha incorretos!')
                 print('\n')
         except Exception as erro:
             print(erro)
-    
+            
+#Cuidador
+
+    def cad(self, usu, pswd, email, cpf):
+        try:
+            sql = "Insert into cuidador(cod, usu, pswd, email, cpf) values('','{}','{}','{}','{}')".format(usu, pswd, email, cpf)
+            self.con.execute(sql)
+            self.db_connection.commit()
+            return "{} Inserido!".format(self.con.rowcount)
+        except Exception as erro:
+            return erro
+        
+    def loginInsert(self, usu, pswd, usuInput, pswdInput):
+        try:
+            sql = 'select * from cuidador'
+            self.con.execute(sql)
+            for (cod, usu, pswd) in self.con:
+                if usu and pswd == usuInput and pswdInput:
+                    return True
+            else:
+                print('\nUsuário ou senha incorretos!')
+            return False
+        except Exception as erro:
+            return erro
+        
+    def loginValid(self, usu, pswd, usuInput, pswdInput):
+        try:
+            if self.loginInsert(usu, pswd, usuInput, pswdInput) == True:
+                print('Logado com Sucesso !')
+                print('\n')
+            else:
+                print('Login e Senha incorretos!')
+                print('\n')
+        except Exception as erro:
+            print(erro)
+            
+
     def tratarData(self, texto):
         separado = texto.split("/")
         dia = separado[0]
